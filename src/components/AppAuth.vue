e2184ba3be0973cdb5806df050ffa3fa6d5a7913
Diana <vyalova.diana@yandex.ru>
2023-03-11 21:30:26 +0300

    added login and register form fields validation

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -57,104 +57,9 @@
                 >Register</a>
             </li>
           </ul>
-
-          <!-- Login Form -->
-          <form v-show="tab === 'login'" >
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <input
-                type="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email"
-              />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <input
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-            </div>
-            <button
-              type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
-            >
-              Submit
-            </button>
-          </form>
-          <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
-            <!-- Name -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Name</label>
-              <vee-field
-                name = "name"
-                type="text"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Name"/>
-              <ErrorMessage class="text-red-600" name="name" />
-            </div>
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <input
-                type="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email"
-              />
-            </div>
-            <!-- Age -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Age</label>
-              <input
-                type="number"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-              />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <input
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-            </div>
-            <!-- Confirm Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Confirm Password</label>
-              <input
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Confirm Password"
-              />
-            </div>
-            <!-- Country -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Country</label>
-              <select
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-              >
-                <option value="USA">USA</option>
-                <option value="Mexico">Mexico</option>
-                <option value="Germany">Germany</option>
-              </select>
-            </div>
-            <!-- TOS -->
-            <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
-              <label class="inline-block">Accept terms of service</label>
-            </div>
-            <button
-              type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
-            >
-              Submit
-            </button>
-          </vee-form>
+          <app-login-form v-if="tab === 'login'" />
+          <app-register-form v-else />
+         
         </div>
       </div>
     </div>
@@ -164,29 +69,26 @@
 <script>
 import { mapState, mapWritableState } from "pinia";
 import useModalStore from "@/stores/modal"
+import AppRegisterForm from "@/components/RegisterForm.vue"
+import AppLoginForm from "@/components/LoginForm.vue"
+
 
   export default {
-      name: 'AppAuth',
-      data() {
+    name: "AppAuth",
+    data() {
         return {
-          tab: "login",
-          schema: {
-            name: "required|min:3|max:100|alphaSpaces",
-            email: "",
-            age: "",
-            password: "",
-            confirm_password: "",
-            country: "",
-            tos: ""
-          }
-        }
-      },
-      computed: {
-        ...mapState(useModalStore,["hiddenClass"]),
-        ...mapWritableState(useModalStore,{
-          modalVisibility: "isOpen"
+            tab: "login",
+        };
+    },
+    computed: {
+        ...mapState(useModalStore, ["hiddenClass"]),
+        ...mapWritableState(useModalStore, {
+            modalVisibility: "isOpen"
         })
-      }
-
-  }
+    },
+    components: {
+      AppRegisterForm,
+      AppLoginForm
+    },
+}
 </script>
