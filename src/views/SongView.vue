31a1ca046c26e8501cad255648d5d64fd00ddb9c
Diana <vyalova.diana@yandex.ru>
2023-04-01 16:07:46 +0300

    Added music player

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -8,6 +8,7 @@
     <div class="container mx-auto flex items-center">
       <!-- Play/Pause Button -->
       <button
+        @click.prevent="newSong(song)"
         type="button"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
       >
@@ -85,8 +86,9 @@
 
 <script>
 import { songsCollection, auth, commentsCollection } from '@/includes/firebase'
-import { mapState } from 'pinia'
+import { mapState, mapActions } from 'pinia'
 import useUserStore from '@/stores/user'
+import usePlayerStore from '@/stores/player'
 
 export default {
   name: 'SongView',
@@ -129,6 +131,7 @@ export default {
     this.getComments()
   },
   methods: {
+    ...mapActions(usePlayerStore,['newSong']),
     async addComment(values, { resetForm }) {
       this.comment_in_submission = true
       this.comment_show_alert = true
