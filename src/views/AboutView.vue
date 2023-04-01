22778eb258d062cd3e658f57ff4deacf2f8ed92d
Diana <vyalova.diana@yandex.ru>
2023-04-01 16:34:30 +0300

    Added comments hash link  and  route transition

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,13 @@
 <template>
-  <h1>About</h1>
+  <main>
+    <h1>About</h1>
   <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum tenetur quo sunt excepturi
     ad labore! Sit cumque voluptatibus aperiam! Laboriosam, eligendi vel itaque ut eius explicabo a?
     Pariatur, deserunt!
   </p>
+  </main>
+  
 </template>
 
 <script>
